7b8cdddb1008cb5a1459fb8796d10f2cbff544d3
Mike Pollitt <mike@limeboy.com>
2015-03-17 21:00:58 +1100

    Progress on resolving partial matches

---
+++ test/helpers/partial_matches_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PartialMatchesHelperTest < ActionView::TestCase
+end
---
+++ test/models/partial_match_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PartialMatchTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
