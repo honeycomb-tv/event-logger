442d1ff5d8b98474a767814b81e05f4a84e75528
Mike Pollitt <mike@limeboy.com>
2015-06-05 20:51:19 +1000

    Fixed issues with database locks, Vantage copying error retry

--- lib/other_background_tasks/validate.rb
+++ lib/other_background_tasks/validate.rb
@@ -3,7 +3,9 @@ class OtherBackgroundTasks::Validate
   @queue = "validate"
 
   def self.perform(h_file_id)
-    h_file = HFile.find(h_file_id)
+    h_file = HFile.find_by_id(h_file_id)
+    return if h_file.nil? # File may have been deleted in the meantime
+
     Rails.logger.info "Validating HFile #{h_file_id}..."
     h_file.begin_validating
     if h_file.valid_sha3?
@@ -11,11 +13,11 @@ class OtherBackgroundTasks::Validate
       h_file.pass_validation
     else
       h_file.fail_validation
-      Rails.logger.info "Failed validation due to mismatched sha3"
+      Rails.logger.warn "Failed validation due to mismatched sha3"
     end
   rescue Exception => exc
     h_file.fail_validation unless h_file.nil?
-    Rails.logger.error "Exception during validation: #{exc.message}"
+    Rails.logger.error "Error validating HFile #{h_file_id}: #{exc.message}"
     Airbrake.notify(exc)
   end
 
